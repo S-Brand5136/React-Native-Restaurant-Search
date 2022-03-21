7811f23a5521d23d0dfb94e42c99bb2a6e223730
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2022-03-20 19:27:31 -0600

    Add base SearchBar component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+const SearchBar = () => {
+  return (
+    <View>
+      <Text>Search Bar</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchBar;
